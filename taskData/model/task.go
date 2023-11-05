1d3d809507eb045b58be302a01ceced5777df7b5
Alex Bondar <abondar1992@gmail.com>
2023-11-05 13:29:48 +0100

    task data: moving task object to shared

---
+++ taskData/model/task.go
@@ -0,0 +1,19 @@
+package model
+
+type Task struct {
+	ID         string     `json:"id"`
+	Name       string     `json:"name"`
+	Status     TaskStatus `json:"status"`
+	CreateTime string     `json:"createdAt"`
+	UpdateTime string     `json:"updatedAt"`
+}
+
+type TaskStatus = string
+
+const (
+	TASK_CREATED TaskStatus = "created"
+	TASK_UPDATED TaskStatus = "updated"
+	TASK_DELETED TaskStatus = "deleted"
+
+	TASK_COMPLETED TaskStatus = "completed"
+)
---
+++ taskStore/model/taskDTO.go
@@ -0,0 +1,6 @@
+package model
+
+type Task struct {
+	ID int64
+	//todo extends
+}
