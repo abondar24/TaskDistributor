731541dec975858787e9683714167e23cbc20c03
Alex Bondar <abondar1992@gmail.com>
2023-11-09 00:12:13 +0100

    task store: added task dao

--- taskStore/dao/taskDAO.go
+++ taskStore/dao/taskDAO.go
@@ -1,16 +1,18 @@
 package dao
 
 import (
+	"database/sql"
+	"fmt"
 	"github.com/abondar24/TaskDistributor/taskData/data"
 	"github.com/abondar24/TaskDistributor/taskStore/model"
+	"log"
 )
 
 type TaskDao interface {
-	SaveTask(task *data.Task) (*model.TaskDTO, error)
+	SaveTask(task *data.Task, tx *sql.Tx) error
 
-	UpdateTask(id *string) error
-	GetTaskById(id *string) (*model.TaskDTO, error)
-	GetTasks(offset *int, limit *int) (*[]model.TaskDTO, error)
+	GetTaskById(id *string, tx *sql.Tx) (*model.TaskDTO, error)
+	GetTasks(offset *int, limit *int, tx *sql.Tx) (*[]*model.TaskDTO, error)
 }
 
 type TaskDaoImpl struct {
@@ -23,17 +25,94 @@ func NewTaskDao(database *Database) *TaskDaoImpl {
 	}
 }
 
-func (dao *TaskDaoImpl) SaveTask(task *data.Task) (*model.TaskDTO, error) {
-	return nil, nil
-}
+func (dao *TaskDaoImpl) SaveTask(task *data.Task, tx *sql.Tx) error {
 
-func (dao *TaskDaoImpl) UpdateTask(id *string) error {
+	query := fmt.Sprintf("INSERT INTO task(id,name,created_at) VALUES ('%v','%v','%v')", task.ID, task.Name, task.CreateTime)
+	stmt, err := tx.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	defer func(stmt *sql.Stmt) {
+		err := stmt.Close()
+		if err != nil {
+			log.Println(err.Error())
+		}
+	}(stmt)
+
+	_, err = stmt.Exec()
+	if err != nil {
+		log.Println(err.Error())
+		return err
+	}
+
 	return nil
 }
 
-func (dao *TaskDaoImpl) GetTaskById(id *string) (*model.TaskDTO, error) {
-	return nil, nil
+func (dao *TaskDaoImpl) GetTaskById(id *string, tx *sql.Tx) (*model.TaskDTO, error) {
+
+	query := fmt.Sprintf("SELECT * FROM task WHERE id='%v'", id)
+
+	result := &model.TaskDTO{}
+
+	stmt, err := tx.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func(stmt *sql.Stmt) {
+		err := stmt.Close()
+		if err != nil {
+			log.Println(err.Error())
+		}
+	}(stmt)
+
+	rows, err := stmt.Query()
+	if err != nil {
+		log.Println(err.Error())
+		return nil, err
+	}
+
+	for rows.Next() {
+		err := rows.Scan(&result.Id, &result.Name, &result.CreatedAt)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return result, nil
 }
-func (dao *TaskDaoImpl) GetTasks(offset *int, limit *int) (*[]model.TaskDTO, error) {
-	return nil, nil
+func (dao *TaskDaoImpl) GetTasks(offset *int, limit *int, tx *sql.Tx) (*[]*model.TaskDTO, error) {
+
+	query := fmt.Sprintf("SELECT * FROM task LIMIT %v OFFSET %v", limit, offset)
+
+	result := make([]*model.TaskDTO, 0)
+
+	stmt, err := tx.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func(stmt *sql.Stmt) {
+		err := stmt.Close()
+		if err != nil {
+			log.Println(err.Error())
+		}
+	}(stmt)
+
+	rows, err := stmt.Query()
+	if err != nil {
+		log.Println(err.Error())
+		return nil, err
+	}
+
+	for rows.Next() {
+		task := model.TaskDTO{}
+		err := rows.Scan(&task.Id, &task.Name, &task.CreatedAt)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &result, nil
 }
--- taskStore/dao/taskHistoryDAO.go
+++ taskStore/dao/taskHistoryDAO.go
@@ -1,19 +1,19 @@
 package dao
 
 import (
+	"database/sql"
 	"github.com/abondar24/TaskDistributor/taskData/data"
 	"github.com/abondar24/TaskDistributor/taskStore/model"
 )
 
 type TaskHistoryDao interface {
-	SaveTaskEntry(entry *model.TaskHistoryDTO) error
+	SaveTaskEntry(entry *model.TaskHistoryDTO, tx *sql.Tx) error
 
-	GetTaskById(id *string) (*model.TaskHistoryDTO, error)
+	GetTaskById(id *string, tx *sql.Tx) (*model.TaskHistoryDTO, error)
+	GetTasksByIds(ids *[]string, tx *sql.Tx) (*[]model.TaskHistoryDTO, error)
 
-	GetTasksByIds(ids *[]string) (*[]model.TaskHistoryDTO, error)
-
-	GetTasksByStatus(status *data.TaskStatus) (*[]model.TaskHistoryDTO, error)
-	GetTaskHistory(id *string, offset *int, limit *int) (*[]model.TaskHistoryDTO, error)
+	GetTasksByStatus(status *data.TaskStatus, tx *sql.Tx) (*[]model.TaskHistoryDTO, error)
+	GetTaskHistory(id *string, offset *int, limit *int, tx *sql.Tx) (*[]model.TaskHistoryDTO, error)
 }
 
 type TaskHistoryDaoImpl struct {
@@ -26,24 +26,24 @@ func NewTaskHistoryDao(database *Database) *TaskHistoryDaoImpl {
 	}
 }
 
-func (dao *TaskHistoryDaoImpl) SaveTaskEntry(entry *model.TaskHistoryDTO) error {
+func (dao *TaskHistoryDaoImpl) SaveTaskEntry(entry *model.TaskHistoryDTO, tx *sql.Tx) error {
 	return nil
 }
 
 // get latest status
-func (dao *TaskHistoryDaoImpl) GetTaskById(id *string) (*model.TaskHistoryDTO, error) {
+func (dao *TaskHistoryDaoImpl) GetTaskById(id *string, tx *sql.Tx) (*model.TaskHistoryDTO, error) {
 	return nil, nil
 }
 
 // get latest status
-func (dao *TaskHistoryDaoImpl) GetTasksByIds(ids *[]string) (*[]model.TaskHistoryDTO, error) {
+func (dao *TaskHistoryDaoImpl) GetTasksByIds(ids *[]string, tx *sql.Tx) (*[]model.TaskHistoryDTO, error) {
 	return nil, nil
 }
 
-func (dao *TaskHistoryDaoImpl) GetTasksByStatus(status *data.TaskStatus) (*[]model.TaskHistoryDTO, error) {
+func (dao *TaskHistoryDaoImpl) GetTasksByStatus(status *data.TaskStatus, tx *sql.Tx) (*[]model.TaskHistoryDTO, error) {
 	return nil, nil
 }
 
-func (dao *TaskHistoryDaoImpl) GetTaskHistory(id *string, offset *int, limit *int) (*[]model.TaskHistoryDTO, error) {
+func (dao *TaskHistoryDaoImpl) GetTaskHistory(id *string, offset *int, limit *int, tx *sql.Tx) (*[]model.TaskHistoryDTO, error) {
 	return nil, nil
 }
