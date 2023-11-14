1e40a98f79ed9413013c7d1f361566a49f330e85
Alex Bondar <abondar1992@gmail.com>
2023-11-14 21:36:42 +0100

    taskData: added status args

---
+++ taskData/args/args.go
@@ -0,0 +1,9 @@
+package args
+
+import "github.com/abondar24/TaskDistributor/taskData/data"
+
+type StatusArgs struct {
+	Status *data.TaskStatus `json:"status"`
+	Offset *int             `json:"offset"`
+	Limit  *int             `json:"limit"`
+}
